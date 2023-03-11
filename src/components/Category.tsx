1918d6da4e163d8325f4989aecddb890045d8328
WiFoDev <lou81804@gmail.com>
2023-03-11 16:18:29 -0500

    feat: add animation to category lable

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,4 +1,5 @@
 import {FC} from "react";
+import {motion} from "framer-motion";
 
 import {ItemList} from "./ItemList";
 
@@ -8,10 +9,22 @@ interface CategoryProps {
   items: {id: string; name: string}[];
 }
 
+const category = {
+  hidden: {translateY: -100, opacity: 0},
+  show: {translateY: 0, opacity: 1},
+};
+
 export const Category: FC<CategoryProps> = ({id, name, items}) => {
   return (
     <li key={id} className="flex flex-col gap-4">
-      <h2 className="text-lg font-medium">{name}</h2>
+      <motion.h2
+        animate="show"
+        className="text-lg font-medium"
+        initial="hidden"
+        variants={category}
+      >
+        {name}
+      </motion.h2>
       <ItemList items={items} />
     </li>
   );
